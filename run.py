a9f6cd9d8eeff194712781519d45e5e7965b7d03
Pauldwyer <pauldwyer55@gmail.com>
2022-10-24 10:05:21 +0000

    add docstring to back_to_start function

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 def intro():
-    """ 
+    """
     Introduction to the game.
     """
     print("Hi There")
     print("")
     name = input("Enter your name:   ")
-    print(f'Greetings {name.capitalize()}! Welcome to Pauls adventure game!')
+    print(f'Greetings {name.capitalize()}! \nWelcome to Pauls adventure game!')
     start = input ("Would you like to play the game? Yes / No\n")
 
 
@@ -17,14 +17,22 @@ def intro():
         back_to_start()
 
 def back_to_start():
-    backToStart = input ("Would you like to go back to the start? y/n \n")
-    if backToStart.lower().strip() == "yes":
+    """
+    Asks the user do they want to go back to the start of the game
+    If yes then the intro runs again
+    If no the user exits the programme
+    """
+    to_the_start = input("Would you like to go back to the start? y/n \n")
+    if to_the_start.lower().strip() == "yes":
         intro()
     else:
         exit()
     
 
 def start_game():
+    """
+    Starts the game
+    """
     print("Starting the game")
 
 
