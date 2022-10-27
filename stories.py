96a21c0130e228690ccb8767eaeb90e13adf036e
Pauldwyer <pauldwyer55@gmail.com>
2022-10-27 12:07:53 +0000

    create falling asleep function in stories.py, call it inside run.py and test

--- stories.py
+++ stories.py
@@ -12,3 +12,41 @@ def bad_ending_function(username):
 
     If you date.
     """)
+
+
+def falling_asleep_function(username):
+    """
+    Falling asleep function
+    """
+    print(f"""
+    --------------------------------------------------------------
+    'Dont hurt her' you shout
+    'Get back or else' you yell at the stranger
+    The stranger slowly steps towards you
+    He has a blade in one hand and a blackjack in the other
+    You turn to Emily and say 'Everything will be ok baby'
+    You turn again back to the stranger, he is edging closer slowly
+    'What do you want' you scream
+    'You must pay for your transgressions' says the stranger in a lowly growl
+    You quickly grab the clock off the fireplace and you charge the stranger
+    You swing with all your strength aiming for his head
+    But he is too fast, he side steps your attemp
+    and cracks you across the head with the blackjack
+    You pass out
+    You awake and find a bloody blade in your hand
+    You throw it across the floor, your hand is covered in blood
+    You look up and scan the room, you see Emily on the floor
+    She is covered in blood
+    The door opens and Emilys father walks in
+    'What in the seven hells have you done' he shouts
+    *Crack*
+    You awake with a splitting headache, you are back in the forest clearing
+    Your tied up against the tree
+    5 men looking down on you
+    You recognise Emilys father Pierre
+    'You thought you could runaway' he says
+    'Now you will die for what you did to my daughter'
+    'Do you have any last words {username} ?'
+    As you open your mouth to protest Pierre slashes our throat
+    You bleed to death tied to the tree
+    """)
